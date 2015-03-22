d9aa01dd5513f73cc06fe406bc09e38ad53e6610
mikes01 <mikolaj0grygiel@gmail.com>
2015-03-22 20:21:29 +0100

    Add show method to trips_controller with mock data

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,15 @@
 class TripsController < ApplicationController
+	@@first_trip={
+		kilometers: 20,
+		average_speed: 30.5,
+		average_RPM: 1200,
+		average_fuel_consumption: 7.4,
+		date: Date.today,
+		user_id: 1
+	}
 	def index
+	end
+	def show
+		render :json=>@@first_trip
 	end
 end
