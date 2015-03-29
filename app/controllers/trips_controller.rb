7b66047d4b67425bb681f45749382c0142557c8a
mikes01 <mikolaj0grygiel@gmail.com>
2015-03-29 20:10:16 +0200

    Service for getting trips group by car type

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -12,9 +12,14 @@ class TripsController < ApplicationController
 
 	def create
 		@trip = Trip.new(params.permit(:distance, :avg_rpm, :avg_fuel, :avg_speed, :date, :user_id))
-		p @trip
 		@trip.save
 		render :json =>@trip
+	end
+
+	def getTripsByCarType
+		trips=Trip.includes(:car_type).where("car_types.engine_type = ?", params.permit(:engine_type)["engine_type"])
+			.where("car_types.engine_displacement = ?", params.permit(:engine_displacement)["engine_displacement"]).references(:car_types)
+		render :json => trips
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   
 	# You can have the root of your site routed with "root"
 	root 'trips#index'
-	resources :trips, only: [:index, :show, :create, :destroy, :update]
+	resources :trips, only: [:index, :show, :create]
+	post 'trips/by_car_type', to: 'trips#getTripsByCarType'
 
 end
