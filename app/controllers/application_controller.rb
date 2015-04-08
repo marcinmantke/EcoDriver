8b72d2066c126cab870261161d14ce09e373801c
mikes01 <mikolaj0grygiel@gmail.com>
2015-04-09 00:27:25 +0200

    Added navbar and divide application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
   end
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if user_signed_in?
+      "application"
+    else
+      "login"
+    end
+  end
 end
