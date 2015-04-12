d7d4b3445a69324e6f60e413a1d459139cae99b0
mikes01 <mikolaj0grygiel@gmail.com>
2015-04-12 16:45:24 +0200

    Response only on json requests at /whoami, /android/registration, /android/update_car_type

--- app/controllers/android/registrations_controller.rb
+++ app/controllers/android/registrations_controller.rb
@@ -1,21 +1,23 @@
 class Android::RegistrationsController < Devise::RegistrationsController
 
-  respond_to :json
   def create
     build_resource(sign_up_params)
 
     if resource.save
       sign_in resource
-      render :status => 200,
-           :json => { :success => true,
+      response = { :success => true,
                       :info => "Registered",
                       :data => resource,
                       :car_type_id => current_user.car_type_id }
     else
-      render :status => :unprocessable_entity,
-             :json => { :success => false,
+      response = { :success => false,
                         :info => resource.errors,
                         :data => {} }
+    end
+
+    respond_to do |format|
+      format.html {  raise ActionController::RoutingError.new('Not Found') }
+      format.json { render json: response }
     end
   end
 
