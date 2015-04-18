8230d8ddae5467153ebe0b8af804b382ee8f209b
mikes01 <mikolaj0grygiel@gmail.com>
2015-04-18 18:33:52 +0200

    Tests for TripsController#create

---
+++ spec/support/controller_macros.rb
@@ -0,0 +1,9 @@
+module ControllerMacros
+  def login_user
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      user = FactoryGirl.build(:user)
+      user.save!(:validate => false)
+      sign_in user
+      @current_user = user
+  end
+end
