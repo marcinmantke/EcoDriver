0dee4f7ad707abe0423669e44fd5fa3d2be38984
Marcin <mentosso@gmail.com>
2015-05-10 12:51:13 +0200

    Seeds and user factory updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-#Trip.create(distance: 1.1, avg_rpm: 1000, avg_fuel: 11, avg_speed: 100, date: Date.today, user_id: 1)
-#Trip.create(distance: 2.1, avg_rpm: 2000, avg_fuel: 12, avg_speed: 110, date: Date.today, user_id: 1)
-#Trip.create(distance: 3.1, avg_rpm: 3000, avg_fuel: 13, avg_speed: 120, date: Date.today, user_id: 1)
-#Trip.create(distance: 4.1, avg_rpm: 4000, avg_fuel: 14, avg_speed: 130, date: Date.today, user_id: 1)
-#Trip.create(distance: 5.1, avg_rpm: 5000, avg_fuel: 15, avg_speed: 140, date: Date.today, user_id: 1)
-
+=begin
 EngineType.create(eng_type: 'diesel', gear_up_min: 2000, gear_up_max: 2200, gear_down: 1700) 
 EngineType.create(eng_type: 'gas', gear_up_min: 2300, gear_up_max: 2500, gear_down: 1800)
 EngineType.create(eng_type: 'petrol', gear_up_min: 2300, gear_up_max: 2500, gear_down: 1800)
@@ -23,3 +18,6 @@ EngineDisplacement.create(disp: '1.6-2.0')
 EngineDisplacement.create(disp: '2.1-2.5')
 EngineDisplacement.create(disp: '2.6-3.0')
 EngineDisplacement.create(disp: '>3.0')
+=end
+
+Trip.update_all(:engine_type_id => rand(1..3),:engine_displacement_id => rand(1..6))
