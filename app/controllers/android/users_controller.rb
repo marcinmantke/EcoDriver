6657dee7947a9a697930725bc71a1f76fac4a563
Marcin <mentosso@gmail.com>
2015-05-13 00:40:55 +0200

    Engine gears service added

--- app/controllers/android/users_controller.rb
+++ app/controllers/android/users_controller.rb
@@ -16,4 +16,20 @@ before_action :authenticate_user!
       format.json { render json: response }
     end
 	end
+
+  def getGearParams
+    if user_signed_in?
+      results=[]
+      results.push({
+        gear_up_min: current_user.engine_type.gear_up_min,
+        gear_up_max: current_user.engine_type.gear_up_max,
+        gear_down: current_user.engine_type.gear_down
+      })
+
+      respond_to do |format|
+          format.html {  raise ActionController::RoutingError.new('Not Found') }
+          format.json { render json: results }
+      end
+    end
+  end
 end
