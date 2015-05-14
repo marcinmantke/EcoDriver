a9ebbeac6270aadce8b97225da5f668c71e76d1b
mikes01 <mikolaj0grygiel@gmail.com>
2015-05-14 15:36:30 +0200

    Corrected rubocop errors:  Style/SingleSpaceBeforeFirstArg

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    username    "Test_user"
-    password    "12345678"
-    email       "t@t.pl"
+    username "Test_user"
+    password "12345678"
+    email "t@t.pl"
     engine_type_id 1
     engine_displacement_id 1
   end
