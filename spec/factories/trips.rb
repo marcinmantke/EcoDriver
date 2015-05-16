1d61ec6d4535f3678dc317f14f2407312381f710
Marcin <mentosso@gmail.com>
2015-05-16 12:55:02 +0200

    Spec trips factory & controller merged

--- spec/factories/trips.rb
+++ spec/factories/trips.rb
@@ -4,7 +4,8 @@ FactoryGirl.define do
     avg_rpm 2500
     avg_speed 94
     avg_fuel 8.9
-    date '12.12.2012'
+    date '2015-05-14T21:23:11.510Z'
+    mark 5.0
   end
 
   factory :trip_with_path, class: Hash do
@@ -12,12 +13,29 @@ FactoryGirl.define do
     avg_rpm 2500
     avg_speed 94
     avg_fuel 8.9
-    date '12.12.2012'
+    date '2015-05-14T21:23:11.510Z'
+	mark 5.0
     path [{ 'longitude' => '12.0',
             'latitude' => '-48.00312' },
           { 'longitude' => '11.0',
             'latitude' => '-47.00312' }]
 
     initialize_with { attributes }
+  end
+
+  factory :full_trip, class: Trip do
+    distance 100
+    avg_rpm 2500
+    avg_speed 94
+    avg_fuel 8.9
+    date '2015-05-14T21:23:11.510Z'
+    mark 5.0
+    #path [{ 'longitude' => '12.0', 'latitude' => '-48.00312' }, { 'longitude' => '11.0', 'latitude' => '-47.00312' }]
+    user_id 1
+    beginning 'Start'
+    finish 'Finish'
+    challenge_id nil
+    engine_type_id 1
+    engine_displacement_id 1
   end
 end
