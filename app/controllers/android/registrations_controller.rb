57b85afb567dca4200c6af4dd9bb281b38f5fdd5
mikes01 <mikolaj0grygiel@gmail.com>
2015-05-16 17:56:34 +0200

    Fixed android login/registration

--- app/controllers/android/registrations_controller.rb
+++ app/controllers/android/registrations_controller.rb
@@ -1,22 +1,20 @@
-class Android
-  class RegistrationsController < Devise::RegistrationsController
-    include ControllerUtil
-    def create
-      build_resource(sign_up_params)
+class Android::RegistrationsController < Devise::RegistrationsController
+  include ControllerUtil
+  def create
+    build_resource(sign_up_params)
 
-      if resource.save
-        sign_in resource
-        response = { success: true,
-                     data: resource,
-                     engine_type_id: current_user.engine_type_id,
-                     engine_displacement_id:
-                      current_user.engine_displacement_id }
-      else
-        response = { success: false,
-                     data: resource.errors }
-      end
-
-      json_respond_formatter(response)
+    if resource.save
+      sign_in resource
+      response = { success: true,
+                   data: resource,
+                   engine_type_id: current_user.engine_type_id,
+                   engine_displacement_id:
+                    current_user.engine_displacement_id }
+    else
+      response = { success: false,
+                   data: resource.errors }
     end
+
+    json_respond_formatter(response)
   end
 end
--- app/controllers/android/users_controller.rb
+++ app/controllers/android/users_controller.rb
@@ -1,28 +1,26 @@
-class Android
-  class UsersController < ApplicationController
-    include ControllerUtil
-    before_action :authenticate_user!
+class Android::UsersController < ApplicationController
+  include ControllerUtil
+  before_action :authenticate_user!
 
-    def update_car_type
-      if user_signed_in?
-        current_user
-          .update(params.permit(:engine_type_id, :engine_displacement_id))
-        response = { success: true }
-      else
-        response = { success: false }
-      end
-
-      json_respond_formatter(response)
+  def update_car_type
+    if user_signed_in?
+      current_user
+        .update(params.permit(:engine_type_id, :engine_displacement_id))
+      response = { success: true }
+    else
+      response = { success: false }
     end
 
-    def gear_params
-      return unless user_signed_in?
-      results = []
-      results.push(gear_up_min: current_user.engine_type.gear_up_min,
-                   gear_up_max: current_user.engine_type.gear_up_max,
-                   gear_down: current_user.engine_type.gear_down)
+    json_respond_formatter(response)
+  end
 
-      json_respond_formatter(response)
-    end
+  def gear_params
+    return unless user_signed_in?
+    results = []
+    results.push(gear_up_min: current_user.engine_type.gear_up_min,
+                 gear_up_max: current_user.engine_type.gear_up_max,
+                 gear_down: current_user.engine_type.gear_down)
+
+    json_respond_formatter(response)
   end
 end
