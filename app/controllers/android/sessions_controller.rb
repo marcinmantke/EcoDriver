4170662a4ea783db7b6ffeba4b6f36b58dbfc8f6
mikes01 <mikolaj0grygiel@gmail.com>
2015-05-16 17:56:34 +0200

    Fixed android login/registration

--- app/controllers/android/sessions_controller.rb
+++ app/controllers/android/sessions_controller.rb
@@ -1,25 +1,23 @@
-class Android
-  class SessionsController < Devise::SessionsController
-    include ControllerUtil
+class Android::SessionsController < Devise::SessionsController
+  include ControllerUtil
 
-    respond_to :json
+  respond_to :json
 
-    def create
-      self.resource = warden.authenticate!(auth_options)
-      sign_in(resource_name, resource)
-      response = { success: true,
-                   data: { engine_type_id: current_user.engine_type_id,
-                           engine_displacement_id:
-                            current_user.engine_displacement_id } }
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    sign_in(resource_name, resource)
+    response = { success: true,
+                 data: { engine_type_id: current_user.engine_type_id,
+                         engine_displacement_id:
+                          current_user.engine_displacement_id } }
 
-      json_respond_formatter(response)
-    end
+    json_respond_formatter(response)
+  end
 
-    def destroy
-      (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-      response = {  success: true, data: false }
+  def destroy
+    (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    response = {  success: true, data: false }
 
-      json_respond_formatter(response)
-    end
+    json_respond_formatter(response)
   end
 end
