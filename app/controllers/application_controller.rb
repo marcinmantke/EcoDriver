4c33fff35d2de1a8032f553543d8a566c2a0a7e3
Marcin <mentosso@gmail.com>
2015-06-04 19:15:40 +0200

    Locale is set based on browser language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -51,7 +51,16 @@ class ApplicationController < ActionController::Base
   end
  
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    I18n.locale = extract_locale_from_accept_language_header || I18n.default_locale
+    logger.debug "* Locale set to '#{I18n.locale}'"
+  end
+ 
+private
+  def extract_locale_from_accept_language_header
+    unless request.env['HTTP_ACCEPT_LANGUAGE'] == nil
+      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    end
   end
 
   def default_url_options(options = {})
