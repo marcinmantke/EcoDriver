406b9f974e30a9b7a78ea0a489fa64997b2a3e8b
mikes01 <mikolaj0grygiel@gmail.com>
2015-06-14 00:32:08 +0200

    Changed Capybara request timeout from 120 to 240

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,10 @@ SimpleCov.formatters = [
 SimpleCov.start
 
 Capybara.register_driver :chrome do |app|
- Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  http_client = Selenium::WebDriver::Remote::Http::Default.new
+  http_client.timeout = 240
+  Capybara::Selenium::Driver.new(app, browser: :chrome,
+                                      http_client: http_client)
 end
 
 Capybara.current_driver = :chrome
@@ -24,6 +27,4 @@ RSpec.configure do |config|
   config.after(:suite) do
     @headless.destroy
   end
-
-
 end
